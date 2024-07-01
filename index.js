3975ea56c2b4a027e49314660015410a48885e21
Lucmor <llmormafer@gmail.com>
2024-07-01 08:44:38 -0300

    Implementação da rota de saslvsr a pergunta

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
 
 app.get('/', (req, res) =>{
     res.render('index')
@@ -11,6 +15,12 @@ app.get('/', (req, res) =>{
 
 app.get('/perguntar', (req, res) =>{
     res.render('perguntar')
+})
+
+app.post('/salvarpergunta', (req, res)=>{
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send(`Titulo: ${titulo} / Descrição: ${descricao}`)
 })
 
 
